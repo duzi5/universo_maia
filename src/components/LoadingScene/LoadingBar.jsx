5e17e717531af87deb4ecc5868466849f29d624a
Antonio Netto <92.nett@gmail.com>
2023-09-19 13:28:11 -0300

    deixando a loading precisando apenas de alguns ajustes

--- src/components/LoadingScene/LoadingBar.jsx
+++ src/components/LoadingScene/LoadingBar.jsx
@@ -1,34 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import "./LoadingBar.scss"
+import React, { useEffect, useState } from "react";
+import "./LoadingBar.scss";
 const LoadingBar = () => {
-    
-    const [progressState, setProgressState ] = useState(0)
-    
- 
-  useEffect(()=>{
-    const interval = setInterval(()=>{
-        setProgressState((prevProgressState)=> prevProgressState + 1)
-    }, 100)
-    
-  } ,[progressState])
-    
-    
+  const [progressState, setProgressState] = useState(0);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setProgressState((prev) => {
+        if (prev >= 100) {
+          clearInterval(interval);
+          return prev;
+        }
+        return prev + 1;
+      });
+    }, 100);
+    return () => clearInterval(interval);
+  }, []);
 
-
-
-    
-    return (
-        <div>
-            <div className="container-loading-bar">
-                <div className="progress-bar" style={{
-                    width: `${progressState}%`
-                }}>
-                                        
-                </div>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div>
+      <div className="container-loading-bar">
+        <div
+          className="progress-bar"
+          style={{
+            width: `${progressState}%`,
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
 
 export default LoadingBar;
--- src/components/LoadingScene/LoadingScene.jsx
+++ src/components/LoadingScene/LoadingScene.jsx
@@ -1,59 +1,76 @@
 import { gsap } from "gsap";
 import { useEffect } from "react";
 import "./LoadScene.scss";
-import LoadingBar from './LoadingBar';
-
+import LoadingBar from "./LoadingBar";
+import whitebutterfly from "./borboletaCogu.svg";
 export default () => {
-    useEffect(() => {
-        const tl = gsap.timeline()
-            tl.to("#initial", {duration: 2, ease: "elastic", opacity:1})
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.to("#initial", { duration: 2, ease: "elastic", opacity: 1 });
 
-        
-        var canvas = document.getElementById('canvas');
-        var ctx = canvas.getContext('2d');
-        canvas.height = window.innerHeight;
-        canvas.width = window.innerWidth;
-        
-        var texts = '0123456789'.split('');
-        var fontSize = 16;
-        var columns = canvas.width / fontSize;
-        var drops = [];
-        for (var x = 0; x < columns; x++) {
-            drops[x] = 1;
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+
+    var texts = "0123456789".split("");
+    var fontSize = 16;
+    var columns = canvas.width / fontSize;
+    var drops = [];
+    for (var x = 0; x < columns; x++) {
+      drops[x] = 1;
+    }
+
+    function draw() {
+      ctx.fillStyle = "rgba(47, 34, 34, 0.05)";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+      ctx.fillStyle = "#4ef1f9";
+      ctx.font = fontSize + "px arial";
+
+      for (var i = 0; i < drops.length; i++) {
+        var text = texts[Math.floor(Math.random() * texts.length)];
+        ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+        if (drops[i] * fontSize > canvas.height || Math.random() > 0.95) {
+          drops[i] = 0;
         }
+        drops[i]++;
+      }
 
-        function draw() {
-            ctx.fillStyle = 'rgba(0,0,0, 0.05)';
-            ctx.fillRect(0, 0, canvas.width, canvas.height);
-            ctx.fillStyle = "#dee0ea";
-            ctx.font = fontSize + 'px arial';
+      requestAnimationFrame(draw);
+    }
 
-            for (var i = 0; i < drops.length; i++) {
-                var text = texts[Math.floor(Math.random() * texts.length)];
-                ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+    gsap.to(".borboleta1", {
+      duration: 10,
+      fill: "red",
+      ease: "easeInOut",
+      repeat: -1,
+      scale: 3,
+      opacity: 0.3,
+      x: 2000,
+      y: 4000,
+      yoyo: true,
+    });
+    gsap.from("#initial",{ 
+        duration: 4,
+        ease: "inOut",
+        opacity: 0,
+        xPercent: 2,
+        yPercent: 3,
+        repeat: -1,
+        
+        })
+    draw();
+  }, []);
 
-                if (drops[i] * fontSize > canvas.height || Math.random() > 0.95) {
-                    drops[i] = 0;
-                }
-                drops[i]++;
-            }
-
-            requestAnimationFrame(draw);
-        }
-
-        draw();
-
-    }, []);
-
-    return (
-      <div className="scene scene1">
-        <div className="conteudo">
-        <div className="spinner">
-        <LoadingBar />ß
+  return (
+    <div className="scene scene1">
+      <div className="conteudo">
         <h5 id="initial"> Você está pronto para viver uma nova realidade?</h5>
-        </div>
-        </div>
-        <canvas id="canvas"></canvas>
+        <LoadingBar />
+        <img className="borboleta1" src={whitebutterfly} />
       </div>
-    );
+      <canvas id="canvas"></canvas>
+    </div>
+  );
 };
