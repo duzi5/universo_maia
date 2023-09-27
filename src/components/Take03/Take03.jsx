a9556caf55ef50067289141044f87ccb718adb1e
Antonio Netto <92.nett@gmail.com>
2023-09-27 05:43:32 -0300

    resolvendo problemas com o take02

--- src/components/Take03/Take03.jsx
+++ src/components/Take03/Take03.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import "./Take03.scss"
 const Take03 = () => {
     return (
-        <div className='take-03'>
+        <div className='take03'>
             <h2>ddfdf</h2>
         </div>
     );
