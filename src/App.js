14dd7f15f134476b784b62a3421e76c8cb678a04
Antonio Netto <92.nett@gmail.com>
2023-10-07 12:30:22 -0300

    finalizando o take08

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Take03 from "./components/Take03/Take03";
 import Take04 from "./components/Take04/Take04";
 import Take05 from './components/Take05/Take05';
 import Take06 from './components/Take06/Take06';
+import Take07 from './components/Take07/Take07';
 
 
 function App() {
@@ -28,6 +29,7 @@ function App() {
       <Take04 />
       <Take05 />
       <Take06 />
+      <Take07 />
     </div>
   );
 }
---
+++ src/components/Take07/Take07.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Take07.scss"
+const Take07 = () => {
+
+    return (
+        <div className="take07">
+            <h5>Expanda a sua mente</h5>
+            <h3>E Mergulhe no <strong>conhecimento ancestral</strong> que liberta o seu futuro</h3>
+            <h2>COGUMELOS</h2>
+        </div>
+    );
+}
+
+export default Take07;
