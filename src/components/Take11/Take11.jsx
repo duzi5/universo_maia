3249cd8a4387ecf3681a5cd09658e33cc61b3105
Antonio Netto <92.nett@gmail.com>
2023-12-10 13:12:48 -0300

    ajustes amanda inssta com link e mudança de cor na borda dos cards do take 09

--- src/components/Take11/Take11.jsx
+++ src/components/Take11/Take11.jsx
@@ -7,7 +7,7 @@ const Take11 = () => {
   return (
     <div id="take11">
       <IoLogoWhatsapp id="whats" />
-      <h1>CONECTE-SE COM NOSSA COMUNIDADE:</h1>
+      <h1>CONECTE-SE COM NOSSA COMUNIDADE:</h>
       <div className="contain">
         <div className="universo-maia contato">
           <h3>Universo Maia</h3>
@@ -15,7 +15,7 @@ const Take11 = () => {
             Dois amigos unidos para trazer informação de qualidade e
             conhecimento sobre essa medicina ancestral que desperta para uma
             vida mais consciente.<br/>
-             @universomaiassa / (71)9.8218-7183
+             <a href="www.instagram.com/universomaiassa">@universomaiassa</a> / (71)9.8218-7183
           </p>
         </div>
         <div className="amanda contato">
@@ -23,7 +23,7 @@ const Take11 = () => {
           <p>
             Mentora Esotérica e Alquimista, com o propósito de expandir a
             compreensão sobre o quanto o autoconhecimento por meio da Magia e
-            Medicina Naturais nos conectam com o presente.<br/> @amandabaragiola / (71)9.8270-5439
+            Medicina Naturais nos conectam com o presente.<br/> <a href="www.instagram.com/amandabaragiola ">@amandabaragiola </a>/ (71)9.8270-5439
           </p>
         </div>
         <Marquee text="No Brasil o cogumelo é descriminalizado. Para maiores informações busque suas referências e mantenha-se atualizado." />
